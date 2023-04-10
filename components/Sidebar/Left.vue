6c63d2d6df653d53ce98d7d9f8cd0d58a271927d
reyhaneh.rad <reyhaneh.rad@haftcin.com>
2023-04-10 18:20:37 +0430

    first commit

---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <div
+      class="p-2 my-2 hover:bg-blue-50 rounded-full w-min dark:hover:bg-white/20"
+      :class="defaultTransition"
+    >
+      <NuxtLink to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </NuxtLink>
+    </div>
+
+    <SidebarLeftTab>
+      <HomeIcon />
+    </SidebarLeftTab>
+  </div>
+</template>
+
+<script setup>
+import { HomeIcon } from "@heroicons/vue/solid";
+const { defaultTransition } = useTailwindConfig();
+</script>
